738af6dda2cb08025543fe83d5386560385bdf1b
Moonlight456 <2321755720@qq.com>
2019-06-17 16:10:08 +0800

    词频统计功能

--- wordcount/views.py
+++ wordcount/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
  #   return HttpRequest('hello')
 def home(request):
     return render(request,'home.html')
+
+def count(request):
+    text=request.GET['text']
+    print(text)
+
+    result={}
+
+    for i in text:
+        if i not in result:
+            result[i]=1
+        else:
+            result[i]+=1
+
+    print(result)
+    result=sorted(result.items(),key=lambda x:x[1],reverse=True)
+    return render(request,'count.html',{'count_result':result})
